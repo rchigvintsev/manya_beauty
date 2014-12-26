34760ec1a97682f955fddd09500fcfbf3f087926
Roman Chigvintsev <rchigvintsev@gmail.com>
2014-12-26 09:45:10 +0300

    Add 'active' CSS class to main menu item corresponding to the current page

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,15 @@
 module StaticPagesHelper
+  def home_page?
+    static_pages_action?('home')
+  end
+
+  def about_page?
+    static_pages_action?('about')
+  end
+
+  private
+
+  def static_pages_action?(action_name)
+    params[:controller] == 'static_pages' && params[:action] == action_name
+  end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,6 +3,18 @@ require 'rails_helper'
 RSpec.describe "StaticPages", :type => :request do
   subject { page }
 
+  describe "main menu" do
+    it "should add 'active' CSS class to current page menu item" do
+      visit root_path
+      should have_selector('li.active', text: I18n.translate('home'))
+      should_not have_selector('li.active', text: I18n.translate('about'))
+
+      visit about_path
+      should have_selector('li.active', text: I18n.translate('about'))
+      should_not have_selector('li.active', text: I18n.translate('home'))
+    end
+  end
+
   describe "Home page" do
     before { visit root_path }
   end
