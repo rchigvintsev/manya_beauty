caad63329ba4dca311f76ded084c8c072f012b9e
Roman Chigvintsev <rchigvintsev@gmail.com>
2015-01-17 16:48:06 +0300

    Add footer with locale links

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,29 +3,78 @@ require 'rails_helper'
 RSpec.describe "StaticPages", :type => :request do
   subject { page }
 
-  describe "main menu" do
-    it "should add 'active' CSS class to current page menu item" do
-      visit root_path
-      should have_selector('li.active', text: I18n.translate('home'))
-      should_not have_selector('li.active', text: I18n.translate('about'))
+  shared_examples_for "all static pages" do
+    describe "header" do
+      it { should have_selector 'div.logo > a > img' }
 
-      visit about_path
-      should have_selector('li.active', text: I18n.translate('about'))
-      should_not have_selector('li.active', text: I18n.translate('home'))
+      it "should have main menu with two items" do
+        expect(find 'ul.main-menu').to have_selector('li', count: 2)
+      end
+    end
+
+    describe "footer" do
+      describe "copyright" do
+        it { should have_content "ManyaBeauty \u00A9 #{Time.now.year}" }
+      end
+
+      describe "locale" do
+        it { should have_link 'RU' }
+        it { should have_link 'EN' }
+      end
     end
   end
 
   describe "Home page" do
     before { visit root_path }
 
+    it_should_behave_like "all static pages"
+
+    describe "main menu" do
+      describe "Home page menu item should be activated" do
+        it { should have_selector('li.active', text: I18n.translate('home')) }
+        it { should_not have_selector('li.active', text: I18n.translate('about')) }
+      end
+    end
+
     describe "greeting" do
       it { should have_content(I18n.translate('greeting')) }
+
+      describe "translation" do
+        describe "for english locale" do
+          before do
+            click_link 'EN'
+          end
+
+          it { should have_content I18n.translate('greeting', locale: 'en') }
+          it { should_not have_content I18n.translate('greeting', locale: 'ru') }
+        end
+
+        describe "for russian locale" do
+          before do
+            click_link 'RU'
+          end
+
+          it { should have_content I18n.translate('greeting', locale: 'ru') }
+          it { should_not have_content I18n.translate('greeting', locale: 'en') }
+        end
+      end
     end
 
     describe "jCarousel" do
-      it { should have_selector('div.jcarousel-wrapper > .jcarousel > ul > li > img') }
-      it { should have_selector('div.jcarousel-wrapper > a.jcarousel-prev') }
-      it { should have_selector('div.jcarousel-wrapper > a.jcarousel-next') }
+      it { should have_selector 'div.jcarousel-wrapper > .jcarousel > ul > li > img' }
+      it { should have_selector 'div.jcarousel-wrapper > a.jcarousel-prev' }
+      it { should have_selector 'div.jcarousel-wrapper > a.jcarousel-next' }
+    end
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    describe "main menu" do
+      describe "About page menu item should be activated" do
+        it { should have_selector('li.active', text: I18n.translate('about')) }
+        it { should_not have_selector('li.active', text: I18n.translate('home')) }
+      end
     end
   end
 end
