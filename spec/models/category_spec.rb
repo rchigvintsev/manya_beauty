ef67de3ff57a659fa3d0edeea9c699c5ed74d8ca
Roman Chigvintsev <rchigvintsev@gmail.com>
2015-02-07 18:45:31 +0300

    Make Category model with appropriate basic controller

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Category, :type => :model do
+  before { @category = Category.new(name: 'Test Category') }
+
+  subject { @category }
+
+  it { should respond_to :name }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+    before { @category.name = " " }
+
+    it { should_not be_valid }
+  end
+
+  describe "when name is already taken" do
+    before do
+      category_with_same_name = @category.dup
+      category_with_same_name.save
+    end
+
+    it { should_not be_valid }
+  end
+end
