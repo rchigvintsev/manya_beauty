a90a9d4f0bfb840164dcaa519b589d3fa3150da6
Roman Chigvintsev <rchigvintsev@gmail.com>
2015-03-12 01:34:43 +0300

    Make some improvements in photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,5 @@
 class Photo < ActiveRecord::Base
   belongs_to :photo_album
 
-  validates :photo_album_id, presence: true
-  validates :title, presence: true
-  validates :file_name, presence: true
+  validates :photo_album_id, :title, :file_name, presence: true
 end
