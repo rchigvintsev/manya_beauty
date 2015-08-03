a47a22689edcece283584a1b18775aa061813a95
Roman Chigvintsev <rchigvintsev@gmail.com>
2015-08-03 14:26:10 +0300

    Add firstname and lastname fields to user model and render sign out link in dashboard page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource_or_scope)
+    admin_dashboard_path
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617144708) do
+ActiveRecord::Schema.define(version: 20150802214709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,8 @@ ActiveRecord::Schema.define(version: 20150617144708) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "is_admin",               default: false, null: false
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
