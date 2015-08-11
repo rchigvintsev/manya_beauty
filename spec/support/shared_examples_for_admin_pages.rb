dd14dcda03f59ea21c9c335864c6df34d34132c1
Roman Chigvintsev <rchigvintsev@gmail.com>
2015-08-11 14:02:31 +0300

    Add ability to switch locale in signin/dashboard page

--- spec/support/shared_examples_for_admin_pages.rb
+++ spec/support/shared_examples_for_admin_pages.rb
@@ -3,5 +3,10 @@ RSpec.shared_examples_for "all admin pages" do
     it { should have_selector 'nav.navbar' }
     it { should have_link I18n.translate('brand'),
         href: root_path(locale: I18n.locale) }
+
+    describe "locale" do
+      it { should have_link 'Русский' }
+      it { should have_link 'English' }
+    end
   end
 end
