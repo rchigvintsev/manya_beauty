ea7c58476b688ef3e9cef2bb9352ebfbcd72259d
Roman Chigvintsev <rchigvintsev@gmail.com>
2015-08-13 16:52:30 +0300

    Add some tests for authentication

--- spec/requests/admin/dashboard_spec.rb
+++ spec/requests/admin/dashboard_spec.rb
@@ -12,4 +12,42 @@ RSpec.describe "Dashboard", :type => :request do
     it { should have_selector 'form #email_input' }
     it { should have_selector 'form #password_input' }
   end
+
+  describe "authentication" do
+    let(:sign_in_button_selector) do
+      "input[type=submit][value='#{I18n.translate('sign_in.form.sign_in')}']"
+    end
+
+    before { visit user_session_path }
+
+    describe "with invalid credentials" do
+      before { click_button I18n.translate('sign_in.form.sign_in') }
+
+      it { should have_content I18n.translate('devise.failure.invalid',
+          authentication_keys: 'email') }
+      it { should_not have_link I18n.translate('sign_out'),
+          href: destroy_user_session_path(locale: I18n.locale) }
+      it { should have_selector sign_in_button_selector }
+    end
+
+    describe "with valid credentials" do
+      let(:user) { FactoryGirl.create(:admin) }
+
+      before do
+        fill_in I18n.translate('sign_in.form.email'), with: user.email
+        fill_in I18n.translate('sign_in.form.password'), with: user.password
+        click_button I18n.translate('sign_in.form.sign_in')
+      end
+
+      it { should have_link I18n.translate('sign_out'),
+          href: destroy_user_session_path(locale: I18n.locale) }
+      it { should_not have_selector sign_in_button_selector }
+
+      describe "followed by sign out" do
+        before { click_link I18n.translate('sign_out') }
+
+        it { should have_selector sign_in_button_selector }
+      end
+    end
+  end
 end
