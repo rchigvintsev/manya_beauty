46e2169a30baad28eebd69e9d86e35430f8c384f
Roman Chigvintsev <rchigvintsev@gmail.com>
2015-08-18 00:06:07 +0300

    Make new category link work

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,32 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!
 
   def index
     @categories = Category.all
   end
 
   def new
+    @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to categories_url,
+            notice: 'Category was successfully created.' }
+        format.json { render :index, status: :created, location: categories_url }
+      else
+        format.html { render :new }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
