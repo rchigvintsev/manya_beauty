c9be84c820474230c6a161e91509ad29e344399c
Roman Chigvintsev <rchigvintsev@gmail.com>
2015-09-18 15:14:33 +0300

    Make new photo link work

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,40 @@
 class PhotosController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_photo, only: [:show, :edit, :update, :destroy]
 
   def index
     @photos = Photo.paginate(page: params[:page])
   end
+
+  def show
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+
+    respond_to do |format|
+      if @photo.save
+        format.html { redirect_to photos_url,
+            notice: I18n.translate('photo.flash.actions.create.notice') }
+        format.json { render :index, status: :created, location: photos_url }
+      else
+        format.html { render :new }
+        format.json { render json: @photo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def set_photo
+      @photo = Photo.find(params[:id])
+    end
+
+    def photo_params
+      params.require(:photo).permit(:title, :description, :photo_file,
+          :photo_album_id)
+    end
 end
