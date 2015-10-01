99183fac138d16c0de95f99a34ac643b32ca9054
Roman Chigvintsev <rchigvintsev@gmail.com>
2015-10-01 17:59:21 +0300

    Add show view to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_comment, only: [:show]
   helper_method :date_time_format
 
   @@date_time_format = '%d %B %Y %H:%M:%S'
@@ -8,7 +9,16 @@ class CommentsController < ApplicationController
     @comments = Comment.order('created_at DESC').paginate(page: params[:page])
   end
 
+  def show
+  end
+
   def date_time_format
     @@date_time_format
   end
+
+  private
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
 end
