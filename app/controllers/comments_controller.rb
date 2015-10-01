b586e3521bde96ea30f6566c8a13e141d6079b39
Roman Chigvintsev <rchigvintsev@gmail.com>
2015-10-01 22:37:53 +0300

    Make edit comment link work

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
+  include PaginationUtils
+
   before_action :authenticate_user!
-  before_action :set_comment, only: [:show]
+  before_action :set_comment, only: [:show, :edit, :update]
   helper_method :date_time_format
 
   @@date_time_format = '%d %B %Y %H:%M:%S'
@@ -12,6 +14,25 @@ class CommentsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+
+        format.html { redirect_to comment_url(@comment, page: current_page),
+            notice: I18n.translate('comment.flash.actions.update.notice') }
+        format.json { render :show, status: :ok,
+            location: comment_url(@comment, page: current_page) }
+      else
+        format.html { render :edit }
+        format.json { render json: @comment.errors,
+            status: :unprocessable_entity }
+      end
+    end
+  end
+
   def date_time_format
     @@date_time_format
   end
@@ -20,5 +41,9 @@ class CommentsController < ApplicationController
 
     def set_comment
       @comment = Comment.find(params[:id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:text)
     end
 end
