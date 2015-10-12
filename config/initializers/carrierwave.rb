122f2d35f2510f8d1415f45167fe530136750db7
Roman Chigvintsev <rchigvintsev@gmail.com>
2015-10-13 01:59:24 +0300

    Upload CarrierWave initializer to remote repository

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,16 @@
+if Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false if Rails.env.test?
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
+    config.fog_directory  = ENV['AWS_BUCKET']
+  end
+end
